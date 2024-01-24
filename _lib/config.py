062975cc035ef357daa95ea57e04e29d054351a4
shjeong1717 <shjeong1717@gmail.com>
2024-01-24 17:07:37 +0900

    config

--- _lib/config.py
+++ _lib/config.py
@@ -7,5 +7,7 @@ _YAML_DIR = _HOME_DIR +"/yaml"
 _NOW_DT = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 _NOW_TS = int(datetime.now().timestamp() * 1000)
 
-_ACCESS_KEY = ""
-_SECRET_KEY = ""
+_ACCESS_KEY = "E7F12FC35D78208D549F"
+_SECRET_KEY = "A2F4A986F53819ADD599803D52BB66595E319263"
+
+DB_INFO = {'host': '192.168.70.145', 'user': 'root', 'passwd': '123qwe!@#', 'db': 'jsh17', 'charset': 'utf8'}
