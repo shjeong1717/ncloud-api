c2fbcf5d87f57c7dfe40125b47821755c111ba14
shjeong1717 <shjeong1717@gmail.com>
2024-01-24 17:04:55 +0900

    Revert "Revert "12412""
    This reverts commit bb79f6d14d29de8abe1b898821107ca8842d321a.

---
+++ _lib/config.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+_HOME_DIR = "/Users/sanghoonjeong/Work/cloud/workspace/ncloud-api"
+_UPLOAD_DIR = _HOME_DIR +"/_upload"
+_YAML_DIR = _HOME_DIR +"/yaml"
+
+_NOW_DT = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+_NOW_TS = int(datetime.now().timestamp() * 1000)
+
+_ACCESS_KEY = ""
+_SECRET_KEY = ""
