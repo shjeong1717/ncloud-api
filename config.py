23d49bb730b927abec85e7119a4b4fa2c56ab11b
JeongSangHoon <43131010+shjeong1717@users.noreply.github.com>
2024-01-30 16:07:29 +0900

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+_HOME_DIR = "/Users/sanghoonjeong/Work/cloud/workspace/ncloud-api"
+_UPLOAD_DIR = _HOME_DIR +"/_upload"
+_YAML_DIR = _HOME_DIR +"/yaml"
+
+_NOW_DT = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+_NOW_TS = int(datetime.now().timestamp() * 1000)
+
+_ACCESS_KEY = "E7F12FC35D78208D549F"
+_SECRET_KEY = "A2F4A986F53819ADD599803D52BB66595E319263"
